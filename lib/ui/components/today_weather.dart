245ae2a8495f9fcb662d8ac26be5f6cfb4b2fb18
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-21 00:14:30 +0600

    add real time temp

--- lib/ui/components/today_weather.dart
+++ lib/ui/components/today_weather.dart
@@ -21,25 +21,97 @@ class TodayWeather extends StatelessWidget {
         SizedBox(
           width: size.width,
           height: size.height * 0.3,
-          child: Column(
-            children: [
-              Text(
-                weatherModel?.location?.name ?? "",
-                style: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-              Text(
-                  DateFormat.yMMMMEEEEd().format(DateTime.parse(
-                      weatherModel?.current?.lastUpdated.toString() ?? "")),
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: const BoxDecoration(
+                color: Colors.white10,
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(10))),
+            child: Column(
+              children: [
+                Text(
+                  weatherModel?.location?.name ?? "",
                   style: const TextStyle(
-                      fontSize: 22,
+                      fontSize: 25,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white)),
-            ],
+                      color: Colors.white),
+                ),
+                Text(
+                    DateFormat.yMMMMEEEEd().format(DateTime.parse(
+                        weatherModel?.current?.lastUpdated.toString() ?? "")),
+                    style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white10,
+                      ),
+                      child: Row(
+                        children: [
+                          Image.network(
+                              "https:${weatherModel?.current?.condition?.icon ?? ""}"),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 2.0),
+                              child: Text(
+                                weatherModel?.current?.tempC
+                                    ?.round()
+                                    .toString() ??
+                                    "",
+                                style: const TextStyle(
+                                    fontSize: 80,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.pink),
+                              ),
+                            ),
+                            const Text(
+                              'o',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.pink
+                                //foreground: Paint()..shader = shader,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          weatherModel?.current?.condition?.text ?? "",
+                          style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                            //foreground: Paint()..shader = shader,
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    //Spacer()
+                  ],
+                ),
+              ],
+            ),
           ),
-        )
+        ),
       ],
     );
   }
