ec484794029bdadf01a05a18a9ecfd4e7da64d96
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-20 23:56:41 +0600

    add Date and time

--- lib/ui/components/today_weather.dart
+++ lib/ui/components/today_weather.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather_bg_null_safety/flutter_weather_bg.dart';
+import 'package:intl/intl.dart';
 import 'package:open_weather_app/model/weather_model.dart';
 
-class TodayWeather extends StatefulWidget {
+class TodayWeather extends StatelessWidget {
   final WeatherModel? weatherModel;
 
   const TodayWeather({super.key, this.weatherModel});
 
   @override
-  State<TodayWeather> createState() => _TodayWeatherState();
-}
-
-class _TodayWeatherState extends State<TodayWeather> {
-  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
     return Stack(
       children: [
         WeatherBg(
           weatherType: WeatherType.heavyRainy,
-          width: MediaQuery.sizeOf(context).width,
-          height: 300,
+          width: size.width,
+          height: size.height * 0.3,
+        ),
+        SizedBox(
+          width: size.width,
+          height: size.height * 0.3,
+          child: Column(
+            children: [
+              Text(
+                weatherModel?.location?.name ?? "",
+                style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                  DateFormat.yMMMMEEEEd().format(DateTime.parse(
+                      weatherModel?.current?.lastUpdated.toString() ?? "")),
+                  style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+            ],
+          ),
         )
       ],
     );
