a27bc5230773d69bf577502edb9d83dfa3b7054b
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-21 02:33:15 +0600

    added tamp show box

---
+++ lib/ui/components/hourly_weather_listitem.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../model/weather_model.dart';
+
+class HourlyWeatherListItem extends StatelessWidget {
+  final Hour? hour;
+
+  const HourlyWeatherListItem({super.key, this.hour});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.all(8),
+      width: 120,
+      decoration: BoxDecoration(
+          color: Colors.white24, borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 8.0),
+                child: Text(hour?.tempC?.round().toString() ??"",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18)),
+              ),
+              Text("o", style: TextStyle(color: Colors.white)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
