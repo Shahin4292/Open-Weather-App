3919b34be62eb0ad88e3791c4d277b8a0342db7f
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-21 02:13:30 +0600

    add Weather By Hours title

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:open_weather_app/services/api_service.dart';
+import 'package:open_weather_app/ui/components/today_weather.dart';
+
+import '../model/weather_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,9 +12,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ApiService apiService = ApiService();
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.blue,
         centerTitle: true,
@@ -18,6 +27,41 @@ class _HomePageState extends State<HomePage> {
           "Weather App",
           style: TextStyle(
               fontWeight: FontWeight.bold, fontSize: 18, color: Colors.white),
+        ),
+      ),
+      body: SafeArea(
+        child: FutureBuilder(
+          future: apiService.getWeatherData("Dhaka"),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              WeatherModel? weatherModel = snapshot.data;
+              return Column(
+                children: [
+                  TodayWeather(
+                    weatherModel: weatherModel,
+                  ),
+                  SizedBox(
+                    height: size.height * 0.01,
+                  ),
+                  const Text(
+                    "Weather By Hours",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              );
+            }
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text("Error has occurred"),
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
         ),
       ),
     );
